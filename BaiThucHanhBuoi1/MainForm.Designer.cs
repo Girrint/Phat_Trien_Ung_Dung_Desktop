b416e88b02e67ad518b029bc3e5d375920498c1e
Girrint <truongphan172@gmail.com>
2025-08-26 18:02:27 +0700

    MainForm: initial commit
    Signed-off-by: Girrint <truongphan172@gmail.com>

---
+++ BaiThucHanhBuoi1/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace BaiThucHanhBuoi1
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtTen = new System.Windows.Forms.TextBox();
+			this.btnOK = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(79, 95);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(93, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Nhập tên của bạn";
+			// 
+			// txtTen
+			// 
+			this.txtTen.Location = new System.Drawing.Point(197, 93);
+			this.txtTen.Name = "txtTen";
+			this.txtTen.Size = new System.Drawing.Size(140, 20);
+			this.txtTen.TabIndex = 1;
+			// 
+			// btnOK
+			// 
+			this.btnOK.Location = new System.Drawing.Point(171, 151);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(81, 27);
+			this.btnOK.TabIndex = 2;
+			this.btnOK.Text = "Xử lý";
+			this.btnOK.UseVisualStyleBackColor = true;
+			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnOK);
+			this.Controls.Add(this.txtTen);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Chương trình đầu tiên";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtTen;
+		private System.Windows.Forms.Button btnOK;
+	}
+}
+
---
+++ BaiThucHanhBuoi1/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiThucHanhBuoi1
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void btnOK_Click(object sender, EventArgs e)
+		{
+			var TenDaNhap = txtTen.Text;
+
+			MessageBox.Show($"Xin chào bạn {TenDaNhap}. Rất vui được gặp bạn.", "Thông điệp chào mừng");
+		}
+	}
+}

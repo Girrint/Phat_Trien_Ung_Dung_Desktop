3f87acaa17066fd8bf4d9d5f289e0cf9df358d59
Girrint <truongphan172@gmail.com>
2025-08-30 09:41:44 +0700

    frmChinh: Class HangHoa
    Signed-off-by: Girrint <truongphan172@gmail.com>

--- BaiTapWindowsForm/frmChinh.cs
+++ BaiTapWindowsForm/frmChinh.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BaiTapWindowsForm
 {
-	public partial class Form1 : Form
+	public partial class frmChinh : Form
 	{
-		public Form1()
+		public frmChinh()
 		{
 			InitializeComponent();
 		}
